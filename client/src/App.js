d04832682d4cd17f529036e24ee897a1f7def076
Elmer asus <mdelefm@gmail.com>
2022-02-03 15:12:45 -0800

    refactor for deployment 2

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import '../src/styles/App.css';
 import Hero from './Hero';
 import Expertise from './Expertise';
 import Insight from './Insight';
-import Material from './Material';
 import Report from './Report';
 import Partners from './Partners';
 import Works from './Works';
